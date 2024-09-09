246b246b1b2936c94d17b78f23c07a6db6390bb5
Berckson Thierry <abreuthierry3@gmail.com>
2024-09-09 16:47:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let money = parseInt(prompt("Quanto de dinheiro ha disponivel?"))
+let option = parseInt(prompt(` Você tem ${money}R$\n opção 1:Adicionar\n opção 2:Remover\n opção 3:Encerrar`))
+while (option !== 3) {
+  switch (option) {
+    case 1:
+      let add = parseInt(prompt('Quanto você quer adicionar?'))
+      money += add
+      break
+    case 2:
+      let minus = parseInt(prompt('Quanto você quer remover?'))
+      money -= minus
+      break
+    default:
+      alert('Opção Invalida')   
+  }
+  option = parseInt(prompt(` Você tem ${money}R$\n opção 1:Adicionar\n opção 2:Remover\n opção 3:Encerrar`))
+}
+//duo = berckson thierry & gustavo texeira

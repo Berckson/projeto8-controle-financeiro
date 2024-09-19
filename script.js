39e6bd151b1381dc5a4ea8957f22c29259f3d5dd
Berckson Thierry <abreuthierry3@gmail.com>
2024-09-19 07:28:10 -0300

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let money = parseInt(prompt("Quanto de dinheiro ha disponivel?"))
 let option = parseInt(prompt(` Você tem ${money}R$\n opção 1:Adicionar\n opção 2:Remover\n opção 3:Encerrar`))
-while (option !== 3) {
+do {
   switch (option) {
     case 1:
       let add = parseInt(prompt('Quanto você quer adicionar?'))
@@ -14,5 +14,5 @@ while (option !== 3) {
       alert('Opção Invalida')   
   }
   option = parseInt(prompt(` Você tem ${money}R$\n opção 1:Adicionar\n opção 2:Remover\n opção 3:Encerrar`))
-}
+} while (option !== 3)
 //duo = berckson thierry & gustavo texeira
